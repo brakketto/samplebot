e1f1e76f711d77194137e77662e27d0447a65ac0
g.festi <g.festi@almaviva.it>
2016-12-20 10:59:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+  
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: '5c38ba62-ecd1-4488-b760-e2397d109fa4',//process.env.MICROSOFT_APP_ID,
+    appPassword: 'wMaKb2iR4niqHnrhLBNoomj'//process.env.MICROSOFT_APP_PASSWORD
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
